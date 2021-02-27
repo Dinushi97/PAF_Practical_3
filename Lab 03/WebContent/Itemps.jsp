10aed668f5123eeec3c1a6318acaecf2e87a0abd
Dinushi97 <keshanidinushi48@gmail.com>
2021-02-27 18:48:29 +0530

    Up to Read method. Read method run successfully.

--- Lab 03/WebContent/Itemps.jsp
+++ Lab 03/WebContent/Itemps.jsp
@@ -26,15 +26,21 @@ if (request.getParameter("itemCode") != null)
 
 </head>
 <body>
-<h1>Items Management</h1>
-<form method="post" action="Itemps.jsp">
- Item code: <input name="itemCode" type="text"><br>
- Item name: <input name="itemName" type="text"><br>
- Item price: <input name="itemPrice" type="text"><br>
- Item description: <input name="itemDesc" type="text"><br>
- <input name="btnSubmit" type="submit" value="Save">
-</form>
-<%
- out.print(session.getAttribute("statusMsg"));
-%></body>
+	<h1>Items Management</h1>
+	<form method="post" action="Itemps.jsp">
+ 	Item code: <input name="itemCode" type="text"><br>
+ 	Item name: <input name="itemName" type="text"><br>
+ 	Item price: <input name="itemPrice" type="text"><br>
+ 	Item description: <input name="itemDesc" type="text"><br>
+ 	<input name="btnSubmit" type="submit" value="Save">
+	</form>
+	<%
+ 	out.print(session.getAttribute("statusMsg"));
+	%>
+	<br>
+	<%
+ 	Item itemObj = new Item();
+ 	out.print(itemObj.readItems());
+	%>
+</body>	
 </html>
--- Lab 03/src/com/Item.java
+++ Lab 03/src/com/Item.java
@@ -1,10 +1,13 @@
 package com;
 
 import java.sql.Connection;
+import java.sql.*;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
 public class Item {
+	
+//Connection Function
 	public Connection connect()
 	{
 	 Connection con = null;
@@ -24,6 +27,8 @@ public class Item {
 	 return con;
 	}
 	
+
+//Insert Function 
 	public String insertItem(String code, String name, String price, String desc)
 	{
 	 String output = "";
@@ -56,4 +61,59 @@ public class Item {
 	 }
 	return output;
 	}
+	
+	
+//ReadItem function
+	public String readItems()
+	{
+	 String output = "";
+	try
+	 {
+	 Connection con = connect();
+	 if (con == null)
+	 {
+	 return "Error while connecting to the database for reading.";
+	 }
+	 // Prepare the html table to be displayed
+	 output = "<table border='1'><tr><th>Item Code</th>"
+	 +"<th>Item Name</th><th>Item Price</th>"
+	 + "<th>Item Description</th>"
+	 + "<th>Update</th><th>Remove</th></tr>";
+	 String query = "select * from item";
+	 Statement stmt = con.createStatement();
+	 ResultSet rs = stmt.executeQuery(query);
+	 // iterate through the rows in the result set
+	 while (rs.next())
+	 {
+	 String itemID = Integer.toString(rs.getInt("itemID"));
+	 String itemCode = rs.getString("itemCode");
+	 String itemName = rs.getString("itemName");
+	 String itemPrice = Double.toString(rs.getDouble("itemPrice"));
+	 String itemDesc = rs.getString("itemDesc");
+	 // Add a row into the html table
+	 output += "<tr><td>" + itemCode + "</td>";
+	 output += "<td>" + itemName + "</td>";
+	 output += "<td>" + itemPrice + "</td>"; 
+	 output += "<td>" + itemDesc + "</td>";
+	 // buttons
+	 output += "<td><input name='btnUpdate' "
+	 + " type='button' value='Update'></td>"
+	 + "<td><form method='post' action='items.jsp'>"
+	 + "<input name='btnRemove' "
+	 + " type='submit' value='Remove'>"
+	 + "<input name='itemID' type='hidden' "
+	 + " value='" + itemID + "'>" + "</form></td></tr>";
+	 }
+	 con.close();
+	 // Complete the html table
+	 output += "</table>";
+	 }
+	catch (Exception e)
+	 {
+	 output = "Error while reading the items.";
+	 System.err.println(e.getMessage());
+	 }
+	return output;
+	}
+
 }
