261a6e99c2ac9153fbc1b82b3181052ae2ce5f6c
Dinushi97 <keshanidinushi48@gmail.com>
2021-03-18 22:06:22 +0530

    After Changing port no mysql workbench.

--- Lab 03/src/com/Item.java
+++ Lab 03/src/com/Item.java
@@ -15,7 +15,7 @@ public class Item {
 		try
 		{
 			Class.forName("com.mysql.jdbc.Driver");
-			con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test","root", "");
+			con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3307/test","root", "");
 			//For testing
 			System.out.print("Successfully connected");
 		}
